d99b6776af856a98f14413eac3a355da0cfebade
Sam Ban <sam.ban@sheerid.com>
2022-12-12 20:45:17 +0000

    fallback for when we don't have .env file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,25 @@
 import dotenv from 'dotenv';
-const config = dotenv.config().parsed;
+let config = dotenv.config().parsed;
+
+if (config == undefined) {
+    config = {
+        PORT: process.env.PORT,
+        CT_PROJECT_KEY: process.env.CT_PROJECT_KEY,
+        CT_CLIENT_ID: process.env.CT_CLIENT_ID,
+        CT_CLIENT_SECRET: process.env.CT_CLIENT_SECRET,
+        CT_SCOPE: process.env.CT_SCOPE,
+        CTP_AUTH_URL: process.env.CTP_AUTH_URL,
+        CTP_API_URL: process.env.CTP_API_URL,
+        CTP_SCOPES: process.env.CTP_SCOPES,
+        SHEERID_TOKEN: process.env.SHEERID_TOKEN,
+        SHEERID_API_URL: process.env.SHEERID_API_URL,
+        URL: process.env.URL,
+        CART_DISCOUNT_ID: process.env.CART_DISCOUNT_ID,
+        REDIS_HOST: process.env.REDIS_HOST,
+        REDIS_PORT: process.env.REDIS_PORT,
+    };
+}
+
+console.log('config', config, process.env.PORT);
 
 export { config };
